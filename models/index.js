c5a262a0b4b2da3265b5b1b310027abefde6de62
Mark Kleinfelder <Kleinfelder@Chester-Copperpot.local>
2021-01-17 11:52:42 -0700

    updates project name mongoose route

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI|| 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/project4");
+                  "mongodb://localhost/project4v2");
 
 
 
